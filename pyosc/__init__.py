80f706357caa506fa388250a05ce54e77abf65c1
Morph Tollon <89318965+Morph-Tollon@users.noreply.github.com>
2026-01-04 17:17:43 +0000

    feat: initialize __init__.py with OSC components and public API

---
+++ pyosc/__init__.py
@@ -0,0 +1,40 @@
+from oscparser import (
+    OSCArg,
+    OSCArray,
+    OSCBlob,
+    OSCChar,
+    OSCDouble,
+    OSCFalse,
+    OSCFloat,
+    OSCFraming,
+    OSCInt,
+    OSCMessage,
+    OSCModes,
+    OSCString,
+    OSCTimeTag,
+    OSCTrue,
+)
+
+from pyosc.call_handler import CallHandler
+from pyosc.dispatcher import Dispatcher
+from pyosc.peer import Peer
+
+__all__ = [
+    "CallHandler",
+    "Dispatcher",
+    "OSCArg",
+    "OSCArray",
+    "OSCBlob",
+    "OSCChar",
+    "OSCDouble",
+    "OSCFalse",
+    "OSCFloat",
+    "OSCFraming",
+    "OSCInt",
+    "OSCMessage",
+    "OSCModes",
+    "OSCString",
+    "OSCTimeTag",
+    "OSCTrue",
+    "Peer",
+]
