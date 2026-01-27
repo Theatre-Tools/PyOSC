94f620489a8bfdc6a51c628acf072bf376ba339e
Morph Tollon <89318965+Morph-Tollon@users.noreply.github.com>
2026-01-27 01:33:12 +0000

    refactor: remove unused dispatcher from peer initialization in simple_message_tcp.py

--- examples/simple_message_tcp.py
+++ examples/simple_message_tcp.py
@@ -1,10 +1,8 @@
-from pyosc import Dispatcher, OSCFraming, OSCInt, OSCMessage, OSCModes, OSCString, Peer
+from pyosc import OSCFraming, OSCInt, OSCMessage, OSCModes, OSCString, Peer
 
-dispatcher = Dispatcher()
 peer = Peer(
     "127.0.0.1",
     8001,
-    dispatcher=dispatcher,
     mode=OSCModes.UDP,
     framing=OSCFraming.OSC10,
     udp_rx_address="127.0.0.1",
