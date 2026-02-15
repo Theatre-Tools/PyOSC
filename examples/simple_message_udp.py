b640fd83d6a9991ea4bd3edd19b8b59022285156
Morph Tollon <89318965+Morph-Tollon@users.noreply.github.com>
2026-02-15 22:46:26 +0000

    docs: Reformatted Peer object in UDP example

--- examples/simple_message_udp.py
+++ examples/simple_message_udp.py
@@ -1,6 +1,13 @@
 from pyosc import OSCFraming, OSCInt, OSCMessage, OSCModes, OSCString, Peer
 
-peer = Peer("127.0.0.1", 8001, mode=OSCModes.UDP, framing=OSCFraming.OSC10, udp_rx_address="127.0.0.1", udp_rx_port=9002)
+peer = Peer(
+    "127.0.0.1",
+    8001,
+    mode=OSCModes.UDP,
+    framing=OSCFraming.OSC10,
+    udp_rx_address="127.0.0.1",
+    udp_rx_port=9002,
+)
 
 message = OSCMessage(
     address="/test/message",
