92e995e125edaa09f12165b59f678e482eba4000
Morph Tollon <89318965+Morph-Tollon@users.noreply.github.com>
2026-06-19 17:52:13 +0100

    feat: add Transport protocol definition for OSC message handling

---
+++ pyosc/transport.py
@@ -0,0 +1,19 @@
+from typing import Protocol
+
+from oscparser import OSCBundle, OSCMessage
+
+from .dispatcher import Dispatcher
+
+
+class Transport(Protocol):
+    def send(self, packet: OSCMessage | OSCBundle):
+        ...
+
+    def receive(self) -> OSCMessage | OSCBundle:
+        ...
+
+    def start(self):
+        ...
+
+    def add_dispatcher(self, dispatcher: 'Dispatcher'):
+        ...
