a3a1b158a00d099fc58bb9a00dff69c54c449a27
Morph Tollon <89318965+Morph-Tollon@users.noreply.github.com>
2026-06-19 17:04:29 +0100

    feat: refactor UDP transport handling with improved connection management and error handling

--- pyosc/udp.py
+++ pyosc/udp.py
@@ -1,23 +1,68 @@
 import socket
-from typing import TYPE_CHECKING
 
-from .exceptions import PeerConfigurationError
+from oscparser import OSCBundle, OSCDecoder, OSCEncoder, OSCFraming, OSCMessage, OSCTransport
 
-if TYPE_CHECKING:
-    from pyosc.peer import Peer
+from .exceptions import PeerConfigurationError, PeerConnectionError
+from .transport import Transport
 
 
-def _begin_udp(self: Peer) -> socket.socket:
-    if self.connection_role:
-        raise PeerConfigurationError("UDP Peers cannot have a connection role assigned")
-    try:
-        if self.bind_address is None:
-            raise PeerConfigurationError("UDP bind address must be specified for UDP Peers")
-        if self.bind_port is None:
-            raise PeerConfigurationError("Bind port must be specified for UDP Peers")
-        udp_connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        udp_connection.bind((self.bind_address, self.bind_port))
-    except OSError as e:
-        raise PeerConfigurationError(f"Could not bind UDP Peer to {self.bind_address}:{self.bind_port} - {e}") from e
-    self._emit_connection_state(True)
-    return udp_connection
+class UDPTransport(Transport):
+    def __init__(self, bind_ip, bind_port, remotes, remote_port, peer, framing: OSCFraming = OSCFraming.OSC10, learning: bool = False):
+        self.peer = peer
+        self.bind_ip = bind_ip
+        self.bind_port = bind_port
+        self.remotes: list = remotes
+        self.remote_port = remote_port
+        self.encoder = OSCEncoder(transport=OSCTransport.UDP, framing=framing)
+        self.decoder = OSCDecoder(transport=OSCTransport.UDP, framing=framing)
+        self.learning = learning
+
+    @classmethod
+    def from_peer(cls, peer):
+        return cls(
+            bind_ip=peer.bind_ip,
+            bind_port=peer.bind_port,
+            remotes=peer.remotes,
+            remote_port=peer.remote_port,
+            peer=peer,
+            framing=peer.framing,
+            learning=peer.learning
+        )
+
+    def _begin_udp(self) -> socket.socket | None:
+        try:
+            conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            conn.bind((self.bind_ip, self.bind_port))
+            self.peer._emit_connection_state(True)
+            self.conn = conn
+        except OSError as e:
+            raise PeerConfigurationError(
+                f"Could not bind UDP Peer to {self.bind_ip}:{self.bind_port} - {e}") from e
+        finally:
+            self.peer._emit_connection_state(False)
+            if hasattr(self, 'conn'):
+                self.conn.close()
+
+    def start(self):
+        self.udp_connection = self._begin_udp()
+
+    def send(self, packet: OSCMessage | OSCBundle):
+        if self.conn is None:
+            raise PeerConnectionError("UDP connection is not established.")
+        encoded_packet = self.encoder.encode(packet)
+        if self.remotes.__len__() == 0:
+            if self.learning:
+                raise PeerConnectionError(
+                    "No remote addresses are known for this UDP peer. Specify a remote address if you want to send messages before receiving messages."
+                )
+        for remote in self.remotes:
+            try:
+                self.conn.sendto(
+                    encoded_packet, (remote.address, remote.port))
+            except OSError as e:
+                raise PeerConnectionError(
+                    f"Could not send UDP packet to {remote.address}:{remote.port} - {e}") from e
+            finally:
+                self.peer._emit_connection_state(False)
+                if hasattr(self, 'conn'):
+                    self.conn.close()
