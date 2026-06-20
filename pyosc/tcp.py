8e7f2433d4cfb4e53c3fc24b9be0402ff334ae04
Morph Tollon <89318965+Morph-Tollon@users.noreply.github.com>
2026-06-20 03:16:53 +0100

    feat: enhance TCPTransport to emit connection state and start listener thread

--- pyosc/tcp.py
+++ pyosc/tcp.py
@@ -94,6 +94,7 @@ class TCPTransport(Transport):
             self.connection_role = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.connection.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
             self.connection.connect((self.remote.address, self.remote.port))
+            self.peer._emit_connection_state(True)
         else:
             raise PeerConnectionError("Remote peer must be provided for initiating connection")
 
@@ -105,6 +106,10 @@ class TCPTransport(Transport):
     def start(self):
         if self.connection_role == ConnectionRole.INITIATING:
             self._initiate_tcp_connection()
+            self.threads._listener_thread = threading.Thread(target=self._tcp_listener, daemon=True)
+            self.threads._listener_thread.start()
 
         else:
             self._bind_tcp_acceptor()
+            self.threads._listener_thread = threading.Thread(target=self._tcp_listener, daemon=True)
+            self.threads._listener_thread.start()
