2e5b23050b50874c747db1d81cf27c1aa38b059e
Morph Tollon <89318965+Morph-Tollon@users.noreply.github.com>
2026-09-08 16:58:58 +0100

    refactor: move OSCArgTypes to a new types.py module and clean up imports

--- pyosc/__init__.py
+++ pyosc/__init__.py
@@ -1,47 +1,11 @@
 from oscparser import (
-    OSCRGBA,
     OSCArg,
-    OSCArray,
-    OSCAtomic,
-    OSCBlob,
     OSCBundle,
-    OSCChar,
-    OSCDouble,
-    OSCFalse,
-    OSCFloat,
     OSCFraming,
-    OSCImpulse,
-    OSCInt,
-    OSCInt64,
     OSCMessage,
-    OSCMidi,
-    OSCNil,
-    OSCString,
-    OSCSymbol,
     OSCTimeTag,
     OSCTransport,
-    OSCTrue,
 )
-
-
-class OSCArgTypes:
-    OSCAtomic = OSCAtomic
-    OSCInt = OSCInt
-    OSCInt64 = OSCInt64
-    OSCFloat = OSCFloat
-    OSCDouble = OSCDouble
-    OSCString = OSCString
-    OSCSymbol = OSCSymbol
-    OSCChar = OSCChar
-    OSCMidi = OSCMidi
-    OSCRGBA = OSCRGBA
-    OSCImpulse = OSCImpulse
-    OSCNil = OSCNil
-    OSCTrue = OSCTrue
-    OSCFalse = OSCFalse
-    OSCBlob = OSCBlob
-    OSCArray = OSCArray
-
 
 from pyosc.exceptions import Exceptions
 from pyosc.peer import Peer
@@ -52,7 +16,6 @@ __all__ = [
     "ConnectionRole",
     "Exceptions",
     "OSCArg",
-    "OSCArgTypes",
     "OSCBundle",
     "OSCFraming",
     "OSCMessage",
