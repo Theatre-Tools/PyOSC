703bfb3921347048978d93ff817a100797e72f64
Morph Tollon <89318965+Morph-Tollon@users.noreply.github.com>
2026-09-08 16:20:21 +0100

    refactor: reorganize OSCArgTypes and update __all__ exports

--- pyosc/__init__.py
+++ pyosc/__init__.py
@@ -23,36 +23,41 @@ from oscparser import (
     OSCTrue,
 )
 
+
+class OSCArgTypes:
+    OSCAtomic = OSCAtomic
+    OSCInt = OSCInt
+    OSCInt64 = OSCInt64
+    OSCFloat = OSCFloat
+    OSCDouble = OSCDouble
+    OSCString = OSCString
+    OSCSymbol = OSCSymbol
+    OSCChar = OSCChar
+    OSCMidi = OSCMidi
+    OSCRGBA = OSCRGBA
+    OSCImpulse = OSCImpulse
+    OSCNil = OSCNil
+    OSCTrue = OSCTrue
+    OSCFalse = OSCFalse
+    OSCBlob = OSCBlob
+    OSCArray = OSCArray
+
+
+from pyosc.exceptions import Exceptions
 from pyosc.peer import Peer
 from pyosc.transport import Bind, ConnectionRole, Remote
-from pyosc.exceptions import Exceptions
 
 __all__ = [
-    "OSCRGBA",
     "Bind",
     "ConnectionRole",
+    "Exceptions",
     "OSCArg",
-    "OSCArray",
-    "OSCAtomic",
-    "OSCBlob",
+    "OSCArgTypes",
     "OSCBundle",
-    "OSCChar",
-    "OSCDouble",
-    "OSCFalse",
-    "OSCFloat",
     "OSCFraming",
-    "OSCImpulse",
-    "OSCInt",
-    "OSCInt64",
     "OSCMessage",
-    "OSCMidi",
-    "OSCNil",
-    "OSCString",
-    "OSCSymbol",
     "OSCTimeTag",
     "OSCTransport",
-    "OSCTrue",
     "Peer",
     "Remote",
-    "Exceptions",
 ]
